4ff3c722408f79fec67ebe5a0cc721d85dde39c3
MatteoGioia <matteo.gioia1999@gmail.com>
2023-01-12 17:24:57 +0100

    Contract: added gas to transaction Contract: added mock review and upvote

--- smart_contract/migrations/1_InitialMigrations.js
+++ smart_contract/migrations/1_InitialMigrations.js
@@ -11,7 +11,9 @@ module.exports = async function (deployer) {
   const contract = instance.contract;
 
   const accounts = await web3.eth.getAccounts();
+  //console.log(accounts)
   const reviewer = accounts[1];
+  const upvoter = accounts[3];
   const server = accounts[2];
 
   await contract.methods.refreshProducts().send({from: server});
@@ -24,10 +26,22 @@ module.exports = async function (deployer) {
   const tokens = await contract.methods.getReviewTokens().call({from: reviewer});
   console.log(`Reviewer has ${tokens} tokens`);
 
-  const products = await contract.methods.getProduct(productId).call({from: reviewer});
-  console.log(products);
+  const product = await contract.methods.getProduct(productId).call({from: reviewer});
+  console.log(product);
 
-  // const reviewHash = 1234;
-  // await contract.methods.makeReview(productId, reviewHash).send({from: reviewer});
+  const reviewHash = 0x1234;
+  //unless 1mln of gas is specified this fails
+  //might need to check config
+  await contract.methods.makeReview(productId, reviewHash).send({from: reviewer, gas: 1000000});
+
+  //check whether review exists
+  const result = await contract.methods.reviewExists(reviewHash).call({from: reviewer});
+  console.log(result)
+
+  //redeem fake tokens for upvoter
+  await contract.methods.obtainReviewToken(upvoter, productId).send({from: server});
+  await contract.methods.upvoteReview(reviewHash).send({from: upvoter});
+  const upv_token = await contract.methods.getProfileTokens().call({from: reviewer});
+  console.log(`Reviewer has ${upv_token} profile tokens`);
 };
 
